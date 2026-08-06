acc4f94f0fbe3a77a590b4776786bfff3cce866d
Dharmendra Hingu <56948864+dbgrs@users.noreply.github.com>
2026-08-06 13:00:02 +0530

    program-1

---
+++ Duplicate_Detector.py
@@ -0,0 +1,16 @@
+
+number = int(input("Enter size  of list : "))
+duplicate = []
+
+
+for i in range(number):
+    val=int(input())
+    duplicate.append(val)
+
+print("Consecutive Number :")
+for i in range(number-1):
+    if duplicate[i] == duplicate[i+1]:
+        print(duplicate[i])
+
+
+
