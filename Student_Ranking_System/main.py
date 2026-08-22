ce3e843b9bea21ac8a409bf58395e5652483e3f5
Dharmendra Hingu <56948864+dbgrs@users.noreply.github.com>
2026-08-22 11:18:01 +0530

    Refactor ranking and reporting functionality

--- Student_Ranking_System/main.py
+++ Student_Ranking_System/main.py
@@ -1,8 +1,14 @@
 from student import Get_Student_Details
-from ranking import Ranking_Student
+from ranking import Get_Ranking_Student, print_student_details
+from report import write_report, read_report
+
 
 Student_Details = Get_Student_Details()
 
-rank = Ranking_Student(Student_Details)
+Student_Details = Get_Ranking_Student(Student_Details)
 
+#print_student_details(Student_Details)
 
+report_file = write_report(Student_Details)
+
+read_report(report_file)
